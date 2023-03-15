48b7e49727e0e8c02411295b4c36551cdfbe7015
Sajid Hussain <sajidmcsbwn@gmail.com>
2023-03-15 10:44:44 +0500

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,27 @@
-import React from "react";
-import Logo from "../images/logo.ico";
-import {Link} from "react-router-dom";
+import React, { useEffect } from "react";
+import {Link, useLocation} from "react-router-dom";
 
 const Navbar = () => {
+const location = useLocation; 
+useEffect( ()=>{
+  
+}, [location]
+
+)
+
   return (    
     <nav className="navbar navbar-expand-lg bg-primary fixed-top">
       <div className="container-fluid"><Link className="navbar-brand" to="/"  style={{color: "white"}}> {/*} <img src={Logo} width="42" height="40" alt="Sj-News" />*/}<strong>SjNews</strong></Link>
         <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
           <div className="navbar-nav" >
-          <Link className="nav-link " aria-current="page" to="/" style={{color: "white"}}>Home</Link>            
-           <Link className="nav-link" to="/business" style={{color: "white"}}> Business</Link>
-           <Link className="nav-link" to="/entertainment" style={{color: "white"}}> Entertainment</Link>
-           <Link className="nav-link" to="/health" style={{color: "white"}}> Health</Link>
-           <Link className="nav-link" to="/science" style={{color: "white"}}> Science</Link>
-           <Link className="nav-link" to="/sports" style={{color: "white"}}> Sports</Link>
-           <Link className="nav-link" to="/technology" style={{color: "white"}}> Technology</Link>
-           <Link className="nav-link" to="/about" style={{color: "white"}}>About</Link>
+          <Link className={`nav-link ${(location.pathname==="/"?"active":"")}`} aria-current="page" to="/" style={{color: "white"}}>Home</Link>            
+           <Link className={`nav-link ${(location.pathname==="/business"?"active":"")}`} to="/business" style={{color: "white"}}> Business</Link>
+           <Link className={`nav-link ${(location.pathname==="/entertainment"?"active":"")}`} to="/entertainment" style={{color: "white"}}> Entertainment</Link>
+           <Link className={`nav-link ${(location.pathname==="/health"?"active":"")}`} to="/health" style={{color: "white"}}> Health</Link>
+           <Link className={`nav-link ${(location.pathname==="/science"?"active":"")}`} to="/science" style={{color: "white"}}> Science</Link>
+           <Link className={`nav-link ${(location.pathname==="/sports"?"active":"")}`} to="/sports" style={{color: "white"}}> Sports</Link>
+           <Link className={`nav-link ${(location.pathname==="/technology"?"active":"")}`} to="/technology" style={{color: "white"}}> Technology</Link>
+           <Link className={`nav-link ${(location.pathname==="about"?"active":"")}`} to="/about" style={{color: "white"}}>About</Link>
           </div>
         </div>
       </div>
